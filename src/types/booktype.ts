aad0709d20e975a75ba26e32a3a878caa66f3727
Taru <taru@naver.com>
2022-04-19 11:57:55 +0900

    feat: 책 타입 추가

---
+++ src/types/booktype.ts
@@ -0,0 +1,12 @@
+export default interface bookType{
+    title:string,
+    link:string,
+    image:string,
+    author:string,
+    price:string,
+    discount:string,
+    publisher:string,
+    pubdate:string,
+    isbn:string,
+    description:string
+}
