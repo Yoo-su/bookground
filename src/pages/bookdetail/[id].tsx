828dcfcd58e4af7e5f51fe7d41ef27b48645f8b8
jayce <jayceyoo@carrotglobal.com>
2022-07-25 23:22:27 +0900

    책 상세 페이지 책 정보부분 구현 & 책 상세정보 isbn으로 조회 api 구현

--- src/pages/bookdetail/[id].tsx
+++ src/pages/bookdetail/[id].tsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+import xml2js from "xml2js";
+import { Oval } from  'react-loader-spinner'
+import Chip from '@mui/material/Chip';
+import Rating from '@mui/material/Rating';
+import Fab from '@mui/material/Fab';
+import {BsHandThumbsUp, BsHandThumbsDown, BsArrowDownCircle} from 'react-icons/bs';
+import { Wrapper, BookWrapper } from './styles';
+import bookType from "../../types/booktype";
 
-export default function bookdetail() {
+export default function BookDetail() {
+  //const router=useRouter();
+  //const {isbn}=router.query;
+
+  //책 정보 변수
+  const [title, setTitle]=useState('');
+  const [desc,setDesc]=useState('');
+  const [author, setAuthor]=useState('');
+  const [publisher, setPublisher]=useState('');
+  const [imgUrl,setImgUrl]=useState('');
+
+  const [upCount,setUpCount]=useState(0);
+  const [downCount, setDownCount] = useState(0);
+
+  //로딩 변수
+  const [bookLoading, setBookLoading] = useState(true);
+
+  const [ratingValue, setRatingValue] = useState<number | null>(0);
+
+  useEffect(()=>{
+    const splited=window.location.href.split('/')
+    const isbn=splited[splited.length-1].split('=')[1].replace('+',' ');
+    axios.get('/api/searchBookByIsbn',{
+      params:{
+        isbn:isbn
+      }
+    }).then(res=>{
+      const parser = new xml2js.Parser();
+      parser.parseString(res.data.item, function (err, result) {
+
+      const {item}=result.rss.channel[0];
+      setTitle(item[0].title[0]);
+      setDesc(item[0].description[0]);
+      setAuthor(item[0].author[0]);
+      setImgUrl(item[0].image[0].substr(0,item[0].image[0].indexOf("?type")))
+      setBookLoading(false);
+    });
+    })
+  },[]);
+
   return (
-    <div>detail</div>
+    <Wrapper>
+      <BookWrapper>
+        {bookLoading ? 
+          <Oval color="#00BFFF" secondaryColor='grey' height={120} width={120} strokeWidth={4} />:
+          <>
+            <div className="imgBox">
+              <div className="backPattern1"></div>
+              <div className="backPattern2"></div>
+              <img src={imgUrl} alt={imgUrl} loading="lazy" />
+            </div>
+
+            <div className="infoBox">
+              <div className="reputation">
+                <div className="chips">
+                  <Chip className="thumbUp"  icon={<BsHandThumbsUp />} label={upCount} color="primary" variant="outlined" clickable  />
+                  <Chip className="thumbDown" icon={<BsHandThumbsDown />} label={downCount} color="primary" variant="outlined" clickable  />
+                </div>
+                <div className="star">
+                  <Rating
+                    name="simple-controlled"
+                    value={ratingValue}
+                    onChange={(event, newValue) => {
+                      setRatingValue(newValue);
+                    }}
+                  />
+                </div>
+              </div>
+              <b className="title">{title}</b>
+              <b className="author">{author}</b>
+              <p className="desc">- {desc}</p>
+            </div>
+            <Fab className="goBoardBtn" variant="extended" color="info">
+              <BsArrowDownCircle />
+              게시판으로 가기 
+            </Fab>
+          </>
+        }
+        
+      </BookWrapper>
+      
+    </Wrapper>
   )
 }
 
--- src/pages/bookdetail/styles.ts
+++ src/pages/bookdetail/styles.ts
@@ -1,3 +1,103 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
-export const Wrapper=styled.div``;
+const floating=keyframes`
+    0%{
+        transform:translateY(0px);
+    }
+    50%{
+        transform:translateY(-10px);
+    }
+    100%{
+        transform:translateY(0px);
+    }
+`
+
+export const Wrapper=styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    padding:2rem 5rem;
+    margin-top:8rem;
+`;
+
+export const BookWrapper=styled.div`
+    display:flex;
+    position:relative;
+    align-items:center;
+    width:100%;
+    padding:1rem 0.5rem;
+    border-radius:1.5rem;
+    justify-content:center;
+
+    .goBoardBtn{
+        position:absolute;
+        bottom:10px;
+        left:45%;
+        animation:${floating} 3s infinite;
+    }
+
+    .imgBox{
+        position:relative;
+
+        .backPattern1{
+            background-color:#CDCDCD;
+            position:absolute;
+            width:250px;
+            height:250px;
+            top:-40px;
+            right:-40px;
+            z-index:-10;
+        }
+
+        .backPattern2{
+            background-color:#5F6D7A;
+            position:absolute;
+            width:50px;
+            height:50px;
+            bottom:-20px;
+            left:-25px;
+            z-index:-10;
+        }
+        img{
+            width:312px;
+            object-fit:cover;
+            box-shadow:5px 5px 12px rgba(0,0,0,0.3);
+        }
+    }
+    
+    .infoBox{
+        display:flex;
+        flex-direction:column;
+        align-items:start;
+        font-family: 'Nanum Myeongjo', serif;
+        margin-left:5rem;
+
+        .reputation{
+            display:flex;
+            flex-direction:column;
+            .chips{
+                display:flex;
+                margin-bottom:0.2rem;
+                
+                .thumbUp{
+                    margin-right:0.5rem;
+                }
+            }
+
+            .star{
+
+            }
+        }
+
+        .title{
+            font-size:28px;
+        }
+        .author{
+            font-size:14px;
+            margin-top:5px;
+        }
+        .desc{
+            font-size:18px;
+        }
+    }
+`
