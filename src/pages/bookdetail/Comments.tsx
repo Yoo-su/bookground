e5f237249329ea0af6a57b829bd7d6797eaa8706
yoosu <rhan0871@naver.com>
2022-07-30 01:16:03 +0900

    페이징용 컴포넌트 구현 & 메인화면 책 검색결과, 책 상세페이지 댓글 페이징

--- src/pages/bookdetail/Comments.tsx
+++ src/pages/bookdetail/Comments.tsx
@@ -1,34 +1,56 @@
 import { useState, useEffect } from 'react';
 import Rating from '@mui/material/Rating';
 import Avatar from '@mui/material/Avatar';
+import { AiOutlineDelete } from 'react-icons/ai'
+import SnackAlert from './SnackAlert';
 import Pagination from '@components/common/Pagination';
 import { CommentsList, Comment } from './styles';
-import { getComments  } from '@api/book';
+import { getComments, deleteComment  } from '@api/book';
 
 interface propsType{
-    isbn:string
+    isbn:string,
+    comments:any[],
+    setComments:any,
+    user:any
 }
 
-export default function Comments({isbn}:propsType){
-    const [userComments, setUserComments] = useState<any[]>([]);
+export default function Comments({isbn,comments,setComments,user}:propsType){
 
     //페이징 변수
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
     const offset = (page - 1) * limit;
 
+    const [showSnackbar, setShowSnackbar] = useState(false);
+    const [snackMsg, setSnackMsg] = useState('');
+
+    const deleteComm=()=>{
+        deleteComment(isbn,user.email).then(res=>{
+            if (res.data.success){
+                setComments(comments.filter(comment=>comment.email!==user.email));
+                setSnackMsg('댓글이 삭제되었습니다')
+                setShowSnackbar(true);
+            }
+            else{
+                setSnackMsg('댓글 삭제중 오류가 발생했습니다');
+                setShowSnackbar(true);
+            }
+        })
+    }
+
     useEffect(()=>{
         getComments(isbn).then(res=>{
             if (!res.data.success) alert('오류 발생')
-            setUserComments(res.data.comments);
+            setComments(res.data.comments);
         })
-    },[]);
+    },[comments]);
     return(
         <CommentsList>
-            {userComments.slice(offset, offset + limit).map((comment,index)=>(
-                <Comment key={comment._id} order={index%3}>
+            {comments.slice(offset, offset + limit).map((comment,index)=>(
+                <Comment key={comment._id} order={index%5} master={comment.email===user.email}>
                     <div className="left">
                         <Avatar src={comment.profileImg} sx={{ width: 56, height: 56 }} />
+                        
                     </div>
                     
                     <div className="right">
@@ -36,6 +58,7 @@ export default function Comments({isbn}:propsType){
                             <b>{comment.name}</b>
                             <Rating defaultValue={comment.rate} readOnly precision={0.5} />
                             <label>{comment.date}</label>
+                            <AiOutlineDelete className='deleteIcon' size={24} onClick={deleteComm} />
                         </div>
 
                         <div className="text">
@@ -44,12 +67,13 @@ export default function Comments({isbn}:propsType){
                     </div>
                 </Comment>
             ))}
-            <Pagination 
-                total={userComments.length}
+            {comments.length>0 && <Pagination 
+                total={comments.length}
                 limit={limit}
                 page={page}
                 setPage={setPage}
-            />
+            />}
+            <SnackAlert open={showSnackbar} setOpen={setShowSnackbar} message={snackMsg} />
         </CommentsList>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,19 @@
-import {useState, useEffect, useCallback} from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import { Oval } from  'react-loader-spinner'
+import { useState, useEffect, useCallback } from 'react';
+import { Oval } from 'react-loader-spinner'
 import SearchInput from '@components/SearchInput';
 import Book from '@components/Book';
+import Pagination from '@components/common/Pagination';
 import styled from 'styled-components';
 import bookType from '../types/booktype';
 
-
-const Wrapper=styled.div`
+const Wrapper = styled.div`
   display:flex;
   flex-direction:column;
   align-items:center;
+  margin-top:10rem;
 `;
 
-const BookContainer=styled.div`
+const BookContainer = styled.div`
     display:flex;
     flex-direction:column;
     justify-content:center;
@@ -25,7 +24,7 @@ const BookContainer=styled.div`
       box-shadow:0 1px 1px rgba(0,0,0,0.2);
       
       b{
-        margin-left:5rem;
+        margin-left:150px;
         font-family: 'IBM Plex Sans KR', sans-serif;
       }
     }
@@ -34,35 +33,46 @@ const BookContainer=styled.div`
       display:flex;
       justify-content:center;
       flex-wrap:wrap;
+      padding:0 50px;
     }
 `;
 
 export default function Home() {
-  const [books, setBooks]=useState([]);
-  const [isLoading, setIsLoading]=useState(false);
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const setBooksFunc=useCallback((books:any)=>{
+  //페이징 변수
+  const [limit, setLimit] = useState(20);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
+  const setBooksFunc = useCallback((books: any) => {
     setBooks(books);
-  },[]);
-  
+  }, []);
+
   return (
     <Wrapper>
-      <SearchInput setBooks={setBooksFunc} setLoading={setIsLoading}/>
-      
-      <BookContainer>
-      {books.length>0 && (
-        <div className='resultCount'>
-          <b>{books.length}개의 검색 결과</b>
-        </div>
-      )}
-      {isLoading===true && <Oval color="#00BFFF" secondaryColor='grey' height={120} width={120} strokeWidth={4} />}
+      <SearchInput setBooks={setBooksFunc} setLoading={setIsLoading} />
 
-      <div className="books">
-      {books.map((book:bookType)=>(
-        <Book {...book} key={book.isbn}/>
-      ))}
-      </div>
-      
+      <BookContainer>
+        {books.length > 0 && (
+          <div className='resultCount'>
+            <b>{books.length}개의 검색 결과</b>
+          </div>
+        )}
+        {isLoading === true && <Oval color="#00BFFF" secondaryColor='grey' height={120} width={120} strokeWidth={4} />}
+
+        <div className="books">
+          {books.slice(offset, offset + limit).map((book: bookType) => (
+            <Book {...book} key={book.isbn} />
+          ))}
+        </div>
+        {books.length>0 && <Pagination 
+                total={books.length}
+                limit={limit}
+                page={page}
+                setPage={setPage}
+          />}
       </BookContainer>
 
     </Wrapper>
