f2ea9ff92bcdbe4f5df9f733997fa4db3f101580
yoosu <rhan0871@naver.com>
2022-07-29 14:49:21 +0900

    책 검색결과 200개까지 가져오도록 수정

--- src/pages/api/books.ts
+++ src/pages/api/books.ts
@@ -13,23 +13,38 @@ export default async function handler(
 
     const { query } = req.query;
     const baseUrl = 'https://openapi.naver.com/v1/search/book.json';
+    const header={
+        'X-Naver-Client-Id': process.env.ID_KEY || '',
+        'X-Naver-Client-Secret': process.env.SECRET_KEY || '',
+        'Access-Control-Allow-Origin': '*'
+    }
     try {
-        const items = await axios.get(baseUrl, {
+        const books1 = axios.get(baseUrl, {
             params: {
                 query: query,
                 display:100
             },
-            headers: {
-                'X-Naver-Client-Id': process.env.ID_KEY || '',
-                'X-Naver-Client-Secret': process.env.SECRET_KEY || '',
-                'Access-Control-Allow-Origin': '*'
-            }
+            headers: header
+        })
+
+        const books2 = axios.get(baseUrl,{
+            params:{
+                query: query,
+                display: 100,
+                start:100
+            },
+            headers: header
         })
         
-        return res.status(200).json({
-            success: true,
-            items: items.data.items
-        })
+        let items:any[]=[];
+        axios.all([books1, books2]).then(axios.spread((...result)=>{
+            items=items.concat(result[0].data.items).concat(result[1].data.items)
+            
+            res.status(200).json({
+                success: true,
+                items: items
+            })
+        }))
 
     } catch (err) {
         console.log(err);
