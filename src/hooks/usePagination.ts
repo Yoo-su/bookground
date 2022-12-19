4e4c62a26a8075e8acd279a596305c2b3555cb44
yoosu <rhan0871@naver.com>
2022-12-19 14:11:47 +0900

    수정

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -6,7 +6,7 @@ const usePagination = (itemsPerPage: number, total: number) => {
     const { currentPage } = useAppSelector(state => state.books);
     const dispatch = useAppDispatch();
 
-    const pageCount = Math.floor(total / itemsPerPage);
+    const pageCount = Math.ceil(total / itemsPerPage);
 
     return {
         currentPage, setCurrentPage: useCallback((newPage: number) => {
